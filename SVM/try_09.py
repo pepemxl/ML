4344472d63502cf4ceba520192397e92363687c2
José Luis Alonzo <joseluisalonzo@golstats.com>
2018-10-10 19:31:01 -0500

    Se agrego la SVM de opencv

---
+++ SVM/try_09.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 11:40:54 2018
+
+@author: pepe
+"""
+
+import numpy as np
+from sklearn.svm import SVC
+import cv2
+from scipy import signal
+import time
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
+from imutils.object_detection import non_max_suppression
+import math
+import scipy.misc
+from skimage.feature import hog
+import os
+import glob
+from sklearn.externals import joblib
+import svm as svmLibsvm
+import pprint
+import pickle
+#from libsvm.svm import *
+#from libsvm.svmutil import *
+#import cPickle
+
+#PATH_POSITIVAS = "/home/pepe/DATOS/imagenes/caras/s1"
+#PATH_NEGATIVAS = "/home/pepe/DATOS/imagenes/caras/s2"
+PATH_POSITIVAS = "/home/pepe/DATOS/imagenes/MuestrasPlayers/Humanos"
+PATH_NEGATIVAS = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanos"
+PATH_POSITIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers/HumanosTrain"
+PATH_NEGATIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanosTrain"
+PATH_POSITIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers/HumanosTest"
+PATH_NEGATIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanosTest"
+MODEL_PATH= "/home/pepe/DATOS/imagenes/MuestrasPlayers"
+MODEL_FILENAME = "./model"
+ 
+hogcv2 = cv2.HOGDescriptor()
+svm_params = dict( kernel_type = cv2.ml.SVM_LINEAR,
+                   svm_type = cv2.ml.SVM_C_SVC,
+                   C=2.67, 
+                   gamma=5.383 )
+
+# This function extract features indepently of size of image
+def hoggify(path,extension,is_color):
+    data=[]
+    lista=glob.glob(os.path.join(path,"*"+extension))
+    lista=np.squeeze(lista)
+    for file in lista:
+        image = cv2.imread(file, is_color)
+#        dim=256
+#        dim = 128
+        dim = 128
+        img = cv2.resize(image, (dim,dim), interpolation = cv2.INTER_AREA)
+        img = hogcv2.compute(img)
+        img = np.squeeze(img)
+        data.append(img)
+    return data
+
+def hoggify2(path,extension,is_color):
+    data=[]
+    lista=glob.glob(os.path.join(path,"*"+extension))
+    lista=np.squeeze(lista)
+    for file in lista:
+        image = cv2.imread(file, is_color)
+        dim = 128
+        img = cv2.resize(image, (dim,dim), interpolation = cv2.INTER_AREA)
+        winSize = (64,64)
+        blockSize = (16,16)
+        blockStride = (8,8)
+        cellSize = (8,8)
+        nbins = 9
+        derivAperture = 1
+        winSigma = 4.
+        histogramNormType = 0
+        L2HysThreshold = 2.0000000000000001e-01
+        gammaCorrection = 0
+        nlevels = 64
+        hog2 = cv2.HOGDescriptor(winSize,blockSize,blockStride,
+                            cellSize,nbins,derivAperture,winSigma,
+                            histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+        img = hog2.compute(img)
+        img = np.squeeze(img)
+        data.append(img)
+    return data
+
+def svm_classify(features,labels):
+    clf=SVC(C=10000,kernel="linear",gamma=0.000001,probability=True)
+    clf.fit(features,labels)
+    joblib.dump(clf, MODEL_PATH+'/'+'modelo_svm_scikit.joblib')
+    with open(MODEL_PATH+'/'+'modelo_svm_scikit.pkl', 'wb') as fileoutput:
+        pickle.dump(clf, fileoutput)
+    return clf
+
+# Hasta donde he revisado no es posible obtener la probabilidad usando svm de opencv
+def svm_classify2(features,labels):
+    svm2=cv2.ml.SVM_create()
+    svm2.setKernel(cv2.ml.SVM_LINEAR)
+    svm2.setType(cv2.ml.SVM_C_SVC)
+    svm2.setC(2.67)
+    svm2.setP(0.2)
+#    svm.train(trainData,responses, params=svm_params)
+    responses = np.array(labels).flatten()
+    trainData = np.array(features)
+#    svm2.train(trainData,responses, params=svm_params)
+    svm2.train(trainData,cv2.ml.ROW_SAMPLE,responses)
+#    svm2.train(features, cv2.ml.ROW_SAMPLE, labels)
+    svm2.save(MODEL_PATH+'/'+'modelo_svm_cv2.dat')
+    return svm2
+
+def svm_classify3(features,labels):
+    svm3=svmLibsvm
+    svm3.setKernel(cv2.ml.SVM_LINEAR)
+    svm3.setType(cv2.ml.SVM_C_SVC)
+    svm3.setC(2.67)
+    svm3.setP(0.2)
+#    svm.train(trainData,responses, params=svm_params)
+    responses = np.array(labels).flatten()
+    trainData = np.array(features)
+#    svm2.train(trainData,responses, params=svm_params)
+    svm3.train(trainData,cv2.ml.ROW_SAMPLE,responses)
+#    svm2.train(features, cv2.ml.ROW_SAMPLE, labels)
+    svm3.save(MODEL_PATH+'/'+'modelo_svm_cv2.dat')
+    return svm2
+
+def list_to_matrix(lst):
+    return np.stack(lst) 
+
+def s_x(img):
+    kernel = np.array([[-1, 0, 1]])
+    imgx = signal.convolve2d(img, kernel, boundary='symm', mode='same')
+    return imgx
+
+def s_y(img):
+    kernel = np.array([[-1, 0, 1]]).T
+    imgy = signal.convolve2d(img, kernel, boundary='symm', mode='same')
+    return imgy
+
+def grad(img):
+    imgx = s_x(img)
+    imgy = s_y(img)
+    s = np.sqrt(imgx**2 + imgy**2)
+    theta = np.arctan2(imgx, imgy)
+    theta[theta<0] = np.pi + theta[theta<0]
+    return (s, theta)
+
+def test_01():
+    dataHogPositivos=hoggify(PATH_POSITIVAS,'jpg',False)
+    dataHogNegativos=hoggify(PATH_NEGATIVAS,'jpg',False)
+    n=len(dataHogPositivos)
+    m=len(dataHogNegativos)
+    dataHog=dataHogPositivos+dataHogNegativos
+    labelsHog=[0]*n+[1]*m
+    clf=svm_classify(dataHog,labelsHog)
+    X_test=dataHogPositivos[-1:]
+    salida=clf.predict(X_test)
+    probability=clf.predict_proba(X_test) # aqui calculo las probabilidades
+    log_probabilities=clf.predict_log_proba(X_test)
+    print("probabilidad 0: %f , 1:%f"%(math.exp(log_probabilities[0,0]),math.exp(log_probabilities[0,1])))
+    X_test=dataHogNegativos[-1:]
+    salida=clf.predict(X_test)
+    probability=clf.predict_proba(X_test) # aqui calculo las probabilidades
+    log_probabilities=clf.predict_log_proba(X_test)
+    print("probabilidad 0: %f , 1:%f"%(math.exp(log_probabilities[0,0]),math.exp(log_probabilities[0,1])))
+    
+def test_02():
+    dataHogPositivos=hoggify(PATH_POSITIVAS_TRAIN,'jpg',False)
+    dataHogNegativos=hoggify(PATH_NEGATIVAS_TRAIN,'jpg',False)
+    n=len(dataHogPositivos)
+    m=len(dataHogNegativos)
+    dataHog=dataHogPositivos+dataHogNegativos
+    labelsHog=[0]*n+[1]*m
+    dataHogPositivosTest=hoggify(PATH_POSITIVAS_TEST,'jpg',False)
+    dataHogNegativosTest=hoggify(PATH_NEGATIVAS_TEST,'jpg',False)
+    nTest=len(dataHogPositivosTest)
+    mTest=len(dataHogNegativosTest)
+    dataHogTest=dataHogPositivosTest+dataHogNegativosTest
+    labelsHogTest=[0]*nTest+[1]*mTest
+    clf=svm_classify(dataHog,labelsHog)
+    predicted=clf.predict(dataHogTest)
+    print("Acurracy score: ",accuracy_score(labelsHogTest, predicted))
+    
+def test_03():
+    dataHogPositivos=hoggify(PATH_POSITIVAS,'jpg',False)
+    dataHogNegativos=hoggify(PATH_NEGATIVAS,'jpg',False)
+    n=len(dataHogPositivos)
+    m=len(dataHogNegativos)
+    dataHog=dataHogPositivos+dataHogNegativos
+    labelsHog=[0]*n+[1]*m
+    svm2=svm_classify2(dataHog,labelsHog)
+    X_test=dataHogPositivos[-1:]
+    salida=svm2.predict(np.array(X_test))[1].ravel()
+    print(salida)
+#    probability=clf.predict_proba(X_test) # aqui calculo las probabilidades
+#    log_probabilities=clf.predict_log_proba(X_test)
+#    print("probabilidad 0: %f , 1:%f"%(math.exp(log_probabilities[0,0]),math.exp(log_probabilities[0,1])))
+#    X_test=dataHogNegativos[-1:]
+#    salida=clf.predict(X_test)
+#    probability=clf.predict_proba(X_test) # aqui calculo las probabilidades
+#    log_probabilities=clf.predict_log_proba(X_test)
+#    print("probabilidad 0: %f , 1:%f"%(math.exp(log_probabilities[0,0]),math.exp(log_probabilities[0,1])))
+    
+    
+if __name__=='__main__':
+    test_02()
+#    test_03()
+#    responses=np.repeat(np.arange(2),250)[:,np.newaxis]
+    dataHogPositivos=hoggify(PATH_POSITIVAS,'jpg',False)
+    dataHogNegativos=hoggify(PATH_NEGATIVAS,'jpg',False)
+    n=len(dataHogPositivos)
+    m=len(dataHogNegativos)
+    dataHog=dataHogPositivos+dataHogNegativos
+    labelsHog=[0]*n+[1]*m
+    svm2=svm_classify2(dataHog,labelsHog)
+    X_test=dataHogPositivos[-1:]
+    salida=svm2.predict(np.array(X_test))[1].ravel()
+    salida=svm2.predict_proba(np.array(X_test))[1].ravel()
+    salida=svm2.predict(np.array(X_test),True)[1].ravel()
+    value=True
+    h1,h2=svm2.predict(np.array(X_test),value)
+    print(value)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(svm2)
+    pp.isreadable(svm2)
+#    print(salida)
