cd8b5095857d19ef88a11d3d06a2c1ec2754acf0
José Luis Alonzo <joseluisalonzo@golstats.com>
2018-10-11 19:42:40 -0500

    NEW: Se inicio el template matching, ahora es necesario agregarleel winSlider

--- SVM/try_09.py
+++ SVM/try_09.py
@@ -31,10 +31,14 @@ import pickle
 #PATH_NEGATIVAS = "/home/pepe/DATOS/imagenes/caras/s2"
 PATH_POSITIVAS = "/home/pepe/DATOS/imagenes/MuestrasPlayers/Humanos"
 PATH_NEGATIVAS = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanos"
-PATH_POSITIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers/HumanosTrain"
-PATH_NEGATIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanosTrain"
-PATH_POSITIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers/HumanosTest"
-PATH_NEGATIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanosTest"
+#PATH_POSITIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers/HumanosTrain"
+#PATH_NEGATIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanosTrain"
+#PATH_POSITIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers/HumanosTest"
+#PATH_NEGATIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers/NoHumanosTest"
+PATH_POSITIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers2/HumanosTrain"
+PATH_NEGATIVAS_TRAIN = "/home/pepe/DATOS/imagenes/MuestrasPlayers2/NoHumanosTrain"
+PATH_POSITIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers2/HumanosTest"
+PATH_NEGATIVAS_TEST = "/home/pepe/DATOS/imagenes/MuestrasPlayers2/NoHumanosTest"
 MODEL_PATH= "/home/pepe/DATOS/imagenes/MuestrasPlayers"
 MODEL_FILENAME = "./model"
  
@@ -147,6 +151,18 @@ def grad(img):
     theta[theta<0] = np.pi + theta[theta<0]
     return (s, theta)
 
+@staticmethod
+def inside(r, q):
+    rx, ry, rw, rh = r
+    qx, qy, qw, qh = q
+    return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
+
+@staticmethod
+def draw_detections(img, rects, thickness = 1):
+    for x, y, w, h in rects:
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
+
 def test_01():
     dataHogPositivos=hoggify(PATH_POSITIVAS,'jpg',False)
     dataHogNegativos=hoggify(PATH_NEGATIVAS,'jpg',False)
@@ -202,27 +218,128 @@ def test_03():
 #    probability=clf.predict_proba(X_test) # aqui calculo las probabilidades
 #    log_probabilities=clf.predict_log_proba(X_test)
 #    print("probabilidad 0: %f , 1:%f"%(math.exp(log_probabilities[0,0]),math.exp(log_probabilities[0,1])))
+
+
+def detector(im,step_size,downscale):
+    im = imutils.resize(im, width = min(400, im.shape[1]))
+    min_wdw_sz = (64, 128)
+#    step_size = (10, 10)
+#    downscale = 1.25
+    clf = joblib.load(os.path.join(model_path, 'svm.model'))
+
+    #List to store the detections
+    detections = []
+    #The current scale of the image 
+    scale = 0
+
+    for im_scaled in pyramid_gaussian(im, downscale = downscale):
+        #The list contains detections at the current scale
+        if im_scaled.shape[0] < min_wdw_sz[1] or im_scaled.shape[1] < min_wdw_sz[0]:
+            break
+        for (x, y, im_window) in sliding_window(im_scaled, min_wdw_sz, step_size):
+            if im_window.shape[0] != min_wdw_sz[1] or im_window.shape[1] != min_wdw_sz[0]:
+                continue
+            im_window = color.rgb2gray(im_window)
+            fd = hog(im_window, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
+
+            fd = fd.reshape(1, -1)
+            pred = clf.predict(fd)
+
+            if pred == 1:
+                
+                if clf.decision_function(fd) > 0.5:
+                    detections.append((int(x * (downscale**scale)), int(y * (downscale**scale)), clf.decision_function(fd), 
+                    int(min_wdw_sz[0] * (downscale**scale)),
+                    int(min_wdw_sz[1] * (downscale**scale))))
+                 
+
+            
+        scale += 1
+
+    clone = im.copy()
+
+    for (x_tl, y_tl, _, w, h) in detections:
+        cv2.rectangle(im, (x_tl, y_tl), (x_tl + w, y_tl + h), (0, 255, 0), thickness = 2)
+
+    rects = np.array([[x, y, x + w, y + h] for (x, y, _, w, h) in detections])
+    sc = [score[0] for (x, y, score, w, h) in detections]
+    print "sc: ", sc
+    sc = np.array(sc)
+    pick = non_max_suppression(rects, probs = sc, overlapThresh = 0.3)
+    print "shape, ", pick.shape
+
+    for(xA, yA, xB, yB) in pick:
+        cv2.rectangle(clone, (xA, yA), (xB, yB), (0, 255, 0), 2)
     
+    plt.axis("off")
+    plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+    plt.title("Raw Detection before NMS")
+    plt.show()
+
+    plt.axis("off")
+    plt.imshow(cv2.cvtColor(clone, cv2.COLOR_BGR2RGB))
+    plt.title("Final Detections after applying NMS")
+    plt.show()
+
+def test_04(videoFilePath,videoFileName):
+    """ Test 04
+    The winStride  parameter is a 2-tuple that dictates the “step size” in both the x and y location of the sliding window.
+    The padding parameter is a tuple which indicates the number of pixels in both the x and y direction in which the sliding window ROI is “padded” prior to HOG feature extraction.
+    """
+    winStride=(8,8)
+#    padding=(32,32)
+    padding=(8,8)
+#    padding=(30,70)
+    locations=((0,0),)
+    scale=1.05
+    iniFrame=0
+    endFrame=200
+    currentSingleFrameNumber=0
+    capture = cv2.VideoCapture(os.path.join(videoFilePath, videoFileName))
+    assert(capture.isOpened())
+    capture.set(cv2.CAP_PROP_POS_FRAMES,currentSingleFrameNumber)
+    for currentSingleFrameNumber in range(iniFrame,int(endFrame)):
+        success, origFrameImg = capture.read()
+        if success:
+            image_results=origFrameImg.copy()
+#            dim = 128
+#            img = cv2.resize(image, (dim,dim), interpolation = cv2.INTER_AREA)
+#            img = hogcv2.compute(img)
+#            img = np.squeeze(img)
+            hist = hogcv2.compute(image_results,winStride,padding,locations)
+            print(hist.shape)
+            print(hist[:36])
+            cv2.imshow('Results',image_results)
+        else:
+            break
+#        key = cv2.waitKey(20)
+        key = cv2.waitKey(0)
+        if key in [27, ord('Q'), ord('q')]:
+            break
+    if capture is not None:
+        capture.release()
+    cv2.destroyAllWindows()
     
 if __name__=='__main__':
-    test_02()
+    test_04('/home/pepe/DATOS/Shared_Videos/TijuanaSantos/folder_of_chunks','chunk_000000.avi')
+#    test_02()
 #    test_03()
 #    responses=np.repeat(np.arange(2),250)[:,np.newaxis]
-    dataHogPositivos=hoggify(PATH_POSITIVAS,'jpg',False)
-    dataHogNegativos=hoggify(PATH_NEGATIVAS,'jpg',False)
-    n=len(dataHogPositivos)
-    m=len(dataHogNegativos)
-    dataHog=dataHogPositivos+dataHogNegativos
-    labelsHog=[0]*n+[1]*m
-    svm2=svm_classify2(dataHog,labelsHog)
-    X_test=dataHogPositivos[-1:]
-    salida=svm2.predict(np.array(X_test))[1].ravel()
-    salida=svm2.predict_proba(np.array(X_test))[1].ravel()
-    salida=svm2.predict(np.array(X_test),True)[1].ravel()
-    value=True
-    h1,h2=svm2.predict(np.array(X_test),value)
-    print(value)
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(svm2)
-    pp.isreadable(svm2)
+#    dataHogPositivos=hoggify(PATH_POSITIVAS,'jpg',False)
+#    dataHogNegativos=hoggify(PATH_NEGATIVAS,'jpg',False)
+#    n=len(dataHogPositivos)
+#    m=len(dataHogNegativos)
+#    dataHog=dataHogPositivos+dataHogNegativos
+#    labelsHog=[0]*n+[1]*m
+#    svm2=svm_classify2(dataHog,labelsHog)
+#    X_test=dataHogPositivos[-1:]
+#    salida=svm2.predict(np.array(X_test))[1].ravel()
+#    salida=svm2.predict_proba(np.array(X_test))[1].ravel()
+#    salida=svm2.predict(np.array(X_test),True)[1].ravel()
+#    value=True
+#    h1,h2=svm2.predict(np.array(X_test),value)
+#    print(value)
+#    pp = pprint.PrettyPrinter(indent=4)
+#    pp.pprint(svm2)
+#    pp.isreadable(svm2)
 #    print(salida)
